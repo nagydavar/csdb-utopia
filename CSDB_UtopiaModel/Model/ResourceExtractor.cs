25bd24b56f9d298b1244c7510ea92e72d75fbe36
Szabadi Botond <szabadiboti@gmail.com>
2026-03-06 10:58:45 +0100

    Added resource extractor

---
+++ CSDB_UtopiaModel/Model/ResourceExtractor.cs
@@ -0,0 +1,9 @@
+using CSDB_UtopiaModel.Persistence;
+
+namespace CSDB_UtopiaModel.Model;
+class ResourceExtractor : Producer
+{
+    public ResourceExtractor(Field, int);
+    public override IndustrialResource Produce();
+    public override Enviromental Require();
+};
