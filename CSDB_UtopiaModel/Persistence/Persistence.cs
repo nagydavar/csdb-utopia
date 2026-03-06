caa685ded0cc13c3e28f6f32f3e9a0f16914082b
Szabadi Botond <szabadiboti@gmail.com>
2026-03-06 11:05:57 +0100

    added basic Persistence getters, setters

--- CSDB_UtopiaModel/Persistence/Persistence.cs
+++ CSDB_UtopiaModel/Persistence/Persistence.cs
@@ -1,23 +1,50 @@
 using CSDB_UtopiaModel.Model;
 
 namespace CSDB_UtopiaModel.Persistence;
-class Persistence{
-        private Field[][] Fields;
-        private List<Town> Towns;
-        private List<Land> Forests;
-        private List<Field> Factories;
-        private List<Field> Producers;
-        private List<Vehicle> VehiclesOnMap;
-        public Dictionary<Resource, int> Storage;
-        public DateTime Date;
-        public int Width;
-        public int Height;
-        public int Budget;
-;
-        public Getters;
-        public Setters;
+class Persistence
+{
+    public List<List<Field>> Fields { get; private set; }
+    public List<Town> Towns { get; private set; }
+    public List<Land> Forests { get; private set; }
+    public List<Factory> Factories { get; private set; }
+    public List<ResourceExtractor> ResourceExtractors { get; private set; }
+    public List<Field> FactoryFields { get; private set; }
+    public List<Field> ResourceExtractorFields { get; private set; }
+    public List<Vehicle<Resource>> VehiclesOnMap { get; private set; }
+    public Dictionary<Resource, int> Storage { get; private set; }
+    public DateTime Date { get; private set; }
+    public int Width { get; private set; }
+    public int Height { get; private set; }
+    public int Budget { get; private set; }
 
-        public void Save();
-        public void Load();
+    public Persistence(int width, int height)
+    {
+        Width = width;
+        Height = height;
 
-    };
+        Fields = new List<List<Field>>();
+        for (int i = 0; i < Width; i++)
+        {
+
+            for (int j = 0; j < Height; j++)
+            {
+                Fields[i][j] = new Land();
+            }
+        }
+    }
+
+    public Persistence GenerateMap(int width, int height)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Save()
+    {
+        throw new NotImplementedException();
+    }
+    public void Load()
+    {
+        throw new NotImplementedException();
+    }
+
+};
