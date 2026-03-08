0fe72372c572a68edbcad934c285fd17e5f77495
Zeitvogel Csaba <qtaq9s@inf.elte.hu>
2026-03-09 00:29:51 +0100

    Create Coal

---
+++ CSDB_UtopiaModel/Model/Coal.cs
@@ -0,0 +1,10 @@
+namespace CSDB_UtopiaModel.Model;
+
+public class Coal : IndustrialResource
+{
+    private static Coal? _instance;
+    
+    public static  Coal Instance => _instance ??= new();
+    
+    protected  Coal() {}
+}
