177c081b64eb08bf7039015b6c264f565b01b7a6
Zeitvogel Csaba <qtaq9s@inf.elte.hu>
2026-03-09 00:29:27 +0100

    Create Gold

---
+++ CSDB_UtopiaModel/Model/Gold.cs
@@ -0,0 +1,10 @@
+namespace CSDB_UtopiaModel.Model;
+
+public class Gold : Treasure
+{
+    private static Gold? _instance;
+    
+    public static Gold Instance => _instance ??= new();
+    
+    protected Gold() {}
+}
