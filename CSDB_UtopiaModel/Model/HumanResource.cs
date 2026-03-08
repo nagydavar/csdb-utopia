03f1c3bc4b825031101d7de9cbb1302dea6d2bca
Zeitvogel Csaba <qtaq9s@inf.elte.hu>
2026-03-09 00:29:15 +0100

    Create HumanResource

--- CSDB_UtopiaModel/Model/HumanResource.cs
+++ CSDB_UtopiaModel/Model/HumanResource.cs
@@ -1,6 +1,10 @@
 namespace CSDB_UtopiaModel.Model;
 
-public class HumanResource
+public class HumanResource : Resource
 {
+    private static HumanResource? _instance;
     
+    public static HumanResource Instance => _instance ??= new ();
+    
+    protected HumanResource() {}
 }
