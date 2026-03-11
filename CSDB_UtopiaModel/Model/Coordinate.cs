97c3d504429ce5401e1280d4aa6438c245953d6c
Zeitvogel Csaba <qtaq9s@inf.elte.hu>
2026-03-11 23:06:02 +0100

    Adding Coordinate

---
+++ CSDB_UtopiaModel/Model/Coordinate.cs
@@ -0,0 +1,33 @@
+using System.Runtime.CompilerServices;
+
+namespace CSDB_UtopiaModel.Model;
+
+public readonly struct Coordinate
+{
+    public int X { get; init; }
+    public int Y { get; init; }
+
+    public Coordinate(int x, int y)
+    {
+        if (x < 0)
+            throw new ArgumentOutOfRangeException(nameof(x), "The parameter must not be non-negative.");
+
+        if (y < 0)
+            throw new ArgumentOutOfRangeException(nameof(y), "The parameter must not be non-negative.");
+
+        X = x;
+        Y = y;
+    }
+
+    public static Coordinate operator +(Coordinate lhs, Coordinate rhs) => new(lhs.X + rhs.X, lhs.Y + rhs.Y);
+
+    public static Coordinate operator -(Coordinate lhs, Coordinate rhs) => new(lhs.X - rhs.X, lhs.Y - rhs.Y);
+
+    public Coordinate Step(Persistence.Up _) => new(X, Y - 1);
+
+    public Coordinate Step(Persistence.Down _) => new(X, Y + 1);
+
+    public Coordinate Step(Persistence.Right _) => new(X + 1, Y);
+
+    public Coordinate Step(Persistence.Left _) => new(X - 1, Y);
+}
