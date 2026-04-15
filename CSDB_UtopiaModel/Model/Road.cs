000dae7d8cfae9011a434f41ab1201eb36951c9a
Zeitvogel Csaba <qtaq9s@inf.elte.hu>
2026-04-15 11:24:26 +0200

    Improve Road's getters

--- CSDB_UtopiaModel/Model/Road.cs
+++ CSDB_UtopiaModel/Model/Road.cs
@@ -5,8 +5,8 @@ namespace CSDB_UtopiaModel.Model;
 public abstract class Road : Buildable, INavigable
 {
     private IVehicle? _leftSide;
-    private IVehicle?  _rightSide;
-    
+    private IVehicle? _rightSide;
+
     public int MaxSpeed { get; set; }
 
     public IVehicle? LeftSide
@@ -14,7 +14,10 @@ public abstract class Road : Buildable, INavigable
         get => _leftSide;
         set
         {
-            if (_leftSide == value) return;
+            if (ReferenceEquals(_leftSide, value)) return;
+            if (_leftSide is not null && value is not null)
+                throw new InvalidOperationException("Left side of the road is occupied");
+
             _leftSide = value;
             if (value is null)
                 Freed?.Invoke(this, new DirectionEventArgs(Left.Instance()));
@@ -26,7 +29,10 @@ public abstract class Road : Buildable, INavigable
         get => _rightSide;
         set
         {
-            if (_rightSide == value) return;
+            if (ReferenceEquals(_rightSide, value)) return;
+            if (_rightSide is not null && value is not null)
+                throw new InvalidOperationException("Right side of the road is occupied");
+
             _rightSide = value;
             if (value is null)
                 Freed?.Invoke(this, new DirectionEventArgs(Right.Instance()));
@@ -54,14 +60,14 @@ public abstract class Road : Buildable, INavigable
         {
             if (dir is not VerticalDirection)
                 throw new Exception();
-            
+
             return dir == Down.Instance() ? LeftSide is null : RightSide is null;
         }
         else
         {
             if (dir is not IHorizontalDirection)
                 throw new Exception();
-            
+
             return dir == Right.Instance() ? LeftSide is null : RightSide is null;
         }
     }
